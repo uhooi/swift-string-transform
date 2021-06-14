84c4168e7041c2b510ac7852dd108cf99046b06f
uhooi <the_uhooi@yahoo.co.jp>
2021-06-14 21:12:56 +0900

    Fix comments

--- Sources/StringTransform/String+Transform.swift
+++ Sources/StringTransform/String+Transform.swift
@@ -2,15 +2,15 @@ import Foundation
 
 extension String {
     
-    /// Transform Hiragana to Katakana
-    /// - Returns: The transformed string
+    /// Transform Hiragana to Katakana.
+    /// - Returns: The transformed string.
     /// - SeeAlso: ``toHiragana()``
     func toKatakana() -> String? {
         applyingTransform(.hiraganaToKatakana, reverse: false)
     }
     
-    /// Transform Katakana to Hiragana
-    /// - Returns: The transformed string
+    /// Transform Katakana to Hiragana.
+    /// - Returns: The transformed string.
     /// - SeeAlso: ``toKatakana()``
     func toHiragana() -> String? {
         applyingTransform(.hiraganaToKatakana, reverse: true)
